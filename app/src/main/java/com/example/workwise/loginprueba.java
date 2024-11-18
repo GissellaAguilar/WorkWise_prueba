aeed12fcae73b9a6c20a2376e97273b821c20a2c
GissellaAguilar <gis.aguilar@duocuc.cl>
2024-11-18 18:52:12 -0300

    login de prueba y sign up, logo nuevo

--- app/src/main/java/com/example/workwise/loginprueba.java
+++ app/src/main/java/com/example/workwise/loginprueba.java
@@ -3,6 +3,7 @@ package com.example.workwise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,8 @@ public class loginprueba extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Intent intent = new Intent(loginprueba.this, MainActivity.class);
+                    startActivity(intent);
                     Toast.makeText(loginprueba.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(loginprueba.this, "Login Failed!", Toast.LENGTH_SHORT).show();
